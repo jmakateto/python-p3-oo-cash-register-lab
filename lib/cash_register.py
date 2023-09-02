de6918061ce18ab4d2c71f256dca3e48a1eec9d3
john makateto <jmakateto@gmail.com>
2023-09-02 23:25:13 +0300

    Implemented CashRegister class

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self):
+        
+        self.items = []  # To store items 
+        self.discount = 0  # To store the discount percentage
+        self.total = 0  # To store the total price
+
+    def add_item(self, item_name, price, quantity):
+        
+        item = (item_name, price, quantity)
+        self.items.append(item)
+        self.total += price * quantity
+
+    def calculate_discount(self, discount_percentage):
+        
+        self.discount = discount_percentage
+        discount_amount = (discount_percentage / 100) * self.total
+        self.total -= discount_amount
+
+    def void_last_transaction(self):
+        
+        if self.items:
+            item_name, price, quantity = self.items.pop()
+            self.total -= price * quantity
